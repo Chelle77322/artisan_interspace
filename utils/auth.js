8c8aa8a20223e1830222a6c3b53c40c237a6069b
Chelle77322 <serenynadarra@gmail.com>
2021-03-17 17:53:10 +1030

    Adding the authentication script

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+const withAuth = (request, result, next) => {
+    //Checks to see if user is logged in. If user is not logged in will be redirected to login 
+
+    if (!ResultSetHeader.session.logged_in){
+        result.redirect('/login');
+    } else{
+        next();
+    }
+};
+module.exports = withAuth;
