90aeccc54780eb505599599aabd9e984962615d0
Chelle77322 <serenynadarra@gmail.com>
2021-03-17 17:59:02 +1030

    Adding the code for the helpers script

---
+++ utils/helpers.js
@@ -0,0 +1,20 @@
+module.exports = {
+    format_date: (date) =>{
+    //Formats the date as MM/DD/YYYY
+    return date.toLocaleDateString();
+    },
+    format_amount: (amount) => {
+        return parseInt(amount).toLocaleString();
+    },
+get_emoji: () =>{
+    const randomNum = Math.random();
+    //Returns a random emoji
+    if(randomNum > 0.7){
+        return `span for = "img" aria-label="monkey"></span`;
+    }else if (randomNum > 0.4) {
+        return `<span for="img" aria-label="laptop">💻</span>`;
+      } else {
+        return `<span for="img" aria-label="gear">⚙️</span>`;
+      }
+    },
+  };
