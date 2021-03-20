931a545782a79d3c491d95e97e3630c6749c95bd
Chelle77322 <serenynadarra@gmail.com>
2021-03-20 21:47:30 +1030

    Fixing heroku connection to database error

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,26 @@
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
+require('dotenv').config({path: __dirname +`/../env`});
 require('config-json');
+
 let sequelize;
 if (process.env.JAWSDB_URL){
     sequelize =  new Sequelize(process.env.JAWSDB_URL);
   } else {
-    sequelize = new Sequelize(
+    sequelize = new Sequelize(//Dialect error
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD, 
+      process.env.DB_HOST,
+      process.env.DIALECT,
         
     {
-        host:'localhost',
-        dialect: 'mysql',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
+        host:process.env.DB_HOST,
+        dialect: 'mysql',//Dialect Error
         port: process.env.PORT
       });
   }
     module.exports = sequelize;
     console.log(sequelize);
-    console.log(host);
+  
     
