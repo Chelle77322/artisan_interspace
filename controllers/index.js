a0ec4dfe8a1f184895a8b2c106227612287662b9
Chelle77322 <serenynadarra@gmail.com>
2021-03-22 12:52:06 +1030

    Working on getting the routes functioning properly

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes.js');
+const artBoard = require('./artboard.js')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('./artboard', artBoard);
+
+router.use((request, result)=> {
+    result.status(404).end();
+
+});
 
 module.exports = router;
