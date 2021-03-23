a4370bff83b513f50125d849e92a94679fb4a2a4
Chelle77322 <serenynadarra@gmail.com>
2021-03-23 11:55:33 +1030

    Javascript coding for art comment handlebars

---
+++ public/jss/art_comment.js
@@ -0,0 +1,24 @@
+async function ArtCommentFormHandler(event){
+    event.preventDefault();
+    const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
+    const artisan_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1 ];
+    
+        if (comment_text){
+            const response = await fetch ('/api/art_comments', {
+                method: 'POST',
+                body: JSON.stringify({
+                    artisan_id,
+                    comment_text
+                }),
+                headers: {'Content-Type': 'application/json'}
+            });
+            if (response.ok){
+                document.location.reload();
+            } else{
+                alert(response.statusText);
+                document.querySelector('#comment-form').style.display = "block";
+            }
+        }
+}
+document.querySelector('.comment-form').addEventListener('submit', ArtCommentFormHandler);
