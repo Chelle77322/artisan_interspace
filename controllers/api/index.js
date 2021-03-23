57a18f523bee18432b152bf6c530fc2954dcb21d
Chelle77322 <serenynadarra@gmail.com>
2021-03-23 10:39:24 +1030

    Adding new model configuration for comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@
 const router = require('express').Router();
 const userRoutes = require ('./userRoutes');
 const artisanRoutes = require('./artisanRoutes');
+const artCommentRoutes = require ('./artCommentRoutes');
 
 
 router.use('/users', userRoutes);
 router.use('/artisans', artisanRoutes);
+router.user ('/artComment', artCommentRoutes);
 
 module.exports = router;
---
+++ models/art_comment.js
@@ -0,0 +1,40 @@
+const {Model, DataTypes} = require ('sequelize');
+const sequelize = require ('../config/connection');
+
+class ArtComment extends Model {}
+
+ArtComment.init({
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    comment_text:{
+        type: DataTypes.STRING,
+        validate:{
+            len:[3]
+        }
+    },
+    user_id:{
+        type:DataTypes.INET,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    artisan_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model: 'artisan',
+            key: 'id'
+        }
+    }
+},{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'art_comment'
+});
+module.exports = ArtComment;
