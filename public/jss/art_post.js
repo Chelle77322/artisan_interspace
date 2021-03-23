05b24caced726a90a19650e46bbd04f0d6aec2d4
Chelle77322 <serenynadarra@gmail.com>
2021-03-23 12:21:27 +1030

    Adding artboad handlebars and js config coding

---
+++ public/jss/art_post.js
@@ -0,0 +1,20 @@
+async function newFormHandler(event){
+    event.preventDefault();
+    const name = document.querySelector('input[name="artisan-title"]').value;
+    const description = document.querySelector('input[name="description"]').value;
+
+    const response = await fetch (`/api/artboard`, {
+        method: 'POST',
+        body: JSON.stringify({
+            name,
+            description
+        }),
+        headers: {'Content-Type': 'application/json'}
+    });
+    if (response.ok){
+        document.location.replace('/artboard');
+    }else{
+        alter(response.statusText);
+    }
+};
+document.querySelector('#new-art-form').addEventListener('submit', newFormHandler);
