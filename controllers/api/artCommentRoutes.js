75dc24902ef1b869de5e6d4df2c5aafc92dfe759
Chelle77322 <serenynadarra@gmail.com>
2021-03-24 12:20:14 +1030

    Adding artComment specific detailed routes

--- controllers/api/artCommentRoutes.js
+++ controllers/api/artCommentRoutes.js
@@ -1,52 +1,67 @@
 const router = require ('express'). Router();
-const {art_comment} = require('../../models');
+const {ArtComment} = require('../../models');
 const WithAuth = require('../../utils/auth');
 
 router.get('/', (request, result) =>{
-  art_comment.findAll({}).then(art_commentData => result.json (art_commentData))
+  ArtComment.findAll({}).then(artcommentData => result.json (artcommentData))
   .catch(error=>{
       console.log(error);
       result.status(500).json(error);
   })
 });
-
+//Gets all ArtComments data based specifically on id
 router.get('/:id', (request, result)=>{
-    art_comment.findAll({
+    ArtComment.findAll({
         where: { id: request.params.id}
     })
-    .then (art_commentData => result.json(art_commentData))
+    .then (artcommentData => result.json(artcommentData))
     .catch(error => {
         console.log(error);
         result.status(500).json(error);
     })
 });
+//Posting a comment
 
-router.put('/:id', WithAuth, (request, result)=>{
-    art_comment.update({
+router.post('/:id', WithAuth, (request, result)=>{
+    ArtComment.create({
+        comment_text: request.body.comment_text,
+        artisan_id: request.body.artisan_id,
+        user_id: request.session.user_id,
+}).then(artcommentData => result.json(artcommentData)).catch(error => {
+    console.log(error);
+    result.status(400).json(error);
+})
+
+});
+//PUT
+router.purge('/:id', WithAuth, (request, result) =>{
+    ArtComment.update({
         comment_text: request.body.comment_text
+
     },{
         where:{id: request.params.id}
-    }).then (art_commentData =>{
-        if(!art_commentData){
-            result.status(404).json({message: 'No comment found with this id'})
+    }).then(artcommentData => {
+        if (artcommentData){
+            result.status(404).json({message: 'No comment was found matching is id'});
             return;
         }
-        result.json(art_commentData);
+        result.json(artcommentData);
     }).catch(error =>{
         console.log(error);
         result.status(500).json(error);
     });
 });
+//DELETE A Comment
 
 router.delete('/:id', WithAuth, (request, result) => {
-    art_comment.destroy({
+    ArtComment.destroy({
         where:{id: request.params.id}
-    }).thne(art_commentData => {
-        if(!art_commentData){
+    }).then(artcommentData => {
+        if(!artcommentData){
             result.status(404).json({message: 'No comment found with this id'});
             return;
         }
-        result.json(art_commentData);
+        result.json(artcommentData);
     }).catch(error =>{
         console.log(error);
         result.status(500).json(error);
