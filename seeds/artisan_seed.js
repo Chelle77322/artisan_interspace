22ee21f4ba2427ceedb72acb35b0c43e4dbfd49d
Chelle77322 <serenynadarra@gmail.com>
2021-04-08 09:57:03 +0930

    Working on handlebars

---
+++ seeds/artisan_seed.js
@@ -0,0 +1,29 @@
+const {Artisan} = require('../models');
+const artboardData = [{
+    name: "Acrylic Pour - Gelato",
+      image:"/css/images/artwork/trial.jpg",
+      description: "An acrylic pour using a tree ring technique with colours of pink, turquoise and rose gold highlights",
+      date_created:"12/04/2020"
+      
+     
+    },
+    {
+      name: "Clouds of Sheep",
+      description: "An abstract interpretation of Australia's wool industry",
+      date_created:"12/04/2020"
+     
+    },
+    {
+      name: "Night stopover",
+      description: "Sponge painting of birds resting in a tree.",
+      date_created:"12/12/2020"
+      
+    },
+    {
+        name: "Acrylic Splatter",
+        description: "Ombre splatter painting",
+        date_created:"1/03/2021"
+       
+      }
+
+    ];
